e0fa3ca38917678caa57f347ae1f04418ebe5269
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2025-01-23 16:52:39 -0500

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -5,15 +5,30 @@ const LOGIN_CREDENTIALS_MISMATCH = 'Email/Password do not match for user.'
 const LOGIN_NONACTIVE_USER_ERROR = '<p>You are NOT an Active user.</p><p>Please contact Supervisor.</p>'
 const PREVENT_LOGIN_CLOSE = true;
 
-function disableEscapeKeyDialogBehavior(event) {
-	if(event.key === 'Escape' && PREVENT_LOGIN_CLOSE) {
-		event.preventDefault();
-	}
-}
-APP_DIALOG.addEventListener('keydown', disableEscapeKeyDialogBehavior);
-//LOGIN_DIALOG.addEventListener('keydown', disableEscapeKeyDialogBehavior);
-//CHOOSE_LOT_DIALOG.addEventListener('keydown', disableEscapeKeyDialogBehavior);
 const CLICK_EVENT = new CustomEvent('click');
+
+const g_MAILBOX_LENGTH = 8;
+const g_TIMEOUT_VAL = 500;
+const g_RESET_TIMEOUT_VAL = 2000;
+const g_IDLE_TIMEOUT_VAL = 60000;// in milliseconds
+const g_APP = 'app';
+const g_NAV = 'nav';
+const g_DIALOG = 'dialog';
+const g_VER = '3.0-Alpha';
+const g_VIN_LENGTH = 17;
+
+const g_CONNECTION_ERROR_COPY = 'An error occured connecting to the data';
+/***** Special Character Replacement Arrays Global Variables */
+const g_SEARCH_ENTITIES = ["Ø", "°",
+						"\"", "\'",
+						"©", "®", "™",
+						"à", "á", "À", "Á",
+						"è", "é", "È", "É"];
+const g_REPLACE_ENTITIES = ["&Oslash;", "&deg;",
+						"&quot;", "&apos;",
+						"&copy;", "&reg;", "&trade;",
+						"&agrave;", "&aacute;", "&Agrave;", "&Aacute;",
+						"&egrave;", "&eacute;", "&Egrave;", "&Eacute;"];
 
 var g_TIMER;
 var g_KEY_RESET_TIMER;
@@ -37,26 +52,11 @@ var g_lot_slots = [];
 g_lot_slots.push( {slot: '001A', full: 0} )
 */
 
-const g_MAILBOX_LENGTH = 8;
-const g_TIMEOUT_VAL = 500;
-const g_RESET_TIMEOUT_VAL = 2000;
-const g_IDLE_TIMEOUT_VAL = 60000;// in milliseconds
-const g_APP = 'app';
-const g_NAV = 'nav';
-const g_DIALOG = 'dialog';
-const g_VER = '3.0-Alpha';
-const g_VIN_LENGTH = 17;
-
-const g_CONNECTION_ERROR_COPY = 'An error occured connecting to the data'
-
-/***** Special Character Replacement Arrays Global Variables */
-const g_SEARCH_ENTITIES = ["Ø", "°",
-						"\"", "\'",
-						"©", "®", "™",
-						"à", "á", "À", "Á",
-						"è", "é", "È", "É"];
-const g_REPLACE_ENTITIES = ["&Oslash;", "&deg;",
-						"&quot;", "&apos;",
-						"&copy;", "&reg;", "&trade;",
-						"&agrave;", "&aacute;", "&Agrave;", "&Aacute;",
-						"&egrave;", "&eacute;", "&Egrave;", "&Eacute;"];
+function disableEscapeKeyDialogBehavior(event) {
+	if(event.key === 'Escape' && PREVENT_LOGIN_CLOSE) {
+		event.preventDefault();
+	}
+}
+APP_DIALOG.addEventListener('keydown', disableEscapeKeyDialogBehavior);
+//LOGIN_DIALOG.addEventListener('keydown', disableEscapeKeyDialogBehavior);
+//CHOOSE_LOT_DIALOG.addEventListener('keydown', disableEscapeKeyDialogBehavior);
