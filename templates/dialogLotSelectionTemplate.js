fe6279a4a6db6b383fa39b40339662985f6bb475
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2025-02-11 15:43:00 -0500

    Update dialogLotSelectionTemplate.js

--- templates/dialogLotSelectionTemplate.js
+++ templates/dialogLotSelectionTemplate.js
@@ -4,11 +4,11 @@ const dialogLotSelectionTemplate = () => {
 					<div id="title">
 						<h2>Lot Selection</h2>
 				</header>
-				<div class="dialog-grid dialog-grid-gap" id="dialog-login-grid">
+				<div class="dialog-grid dialog-grid-gap" id="dialog-lot-grid">
 					<div class="dialog-form-element">
 						<input type="file" id="lot_file" name="lot_file" />
 					</div>
-					<p class="dialog-error invisible" id="dialog-login-error"></p>
+					<p class="dialog-error invisible" id="dialog-lot-error"></p>
 				</div>`;
 	return temp_html;
 }
@@ -22,22 +22,22 @@ const lotFileSelected = (e) => {
 	const reader = new FileReader();
 
 	if(fileExt !== g_LOT_CONFIG_EXT) {
-		document.getElementById('dialog-login-error').textContent = "Unsupportted config file type.";
-		feedBackColoring(document.getElementById('dialog-login-error').id, 'red');
-		document.getElementById('dialog-login-error').classList.remove('invisible');
+		document.getElementById('dialog-lot-error').textContent = "Unsupportted config file type.";
+		feedBackColoring(document.getElementById('dialog-lot-error').id, 'red');
+		document.getElementById('dialog-lot-error').classList.remove('invisible');
 	} else {
-		document.getElementById('dialog-login-error').textContent = '';
-		document.getElementById('dialog-login-error').classList.add('invisible');
-		feedBackColoring(document.getElementById('dialog-login-error').id);
+		document.getElementById('dialog-lot-error').textContent = '';
+		document.getElementById('dialog-lot-error').classList.add('invisible');
+		feedBackColoring(document.getElementById('dialog-lot-error').id);
 
 		reader.addEventListener('load', (e) => {
 			getLot(e.target.result);
 		});
 
 		reader.onerror = () => {
-			document.getElementById('dialog-login-error').textContent = "Error reading the file. Please try again.";
-			feedBackColoring(document.getElementById('dialog-login-error').id, 'red');
-			document.getElementById('dialog-login-error').classList.remove('invisible');
+			document.getElementById('dialog-lot-error').textContent = "Error reading the file. Please try again.";
+			feedBackColoring(document.getElementById('dialog-lot-error').id, 'red');
+			document.getElementById('dialog-lot-error').classList.remove('invisible');
 		};
 
 		reader.readAsText(fileChosen);
