afe4741a0b1d4aef9599269345cf56c9dfab5b60
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2025-02-12 16:37:14 -0500

    Update getLotSlots.php

--- includes/getLotSlots.php
+++ includes/getLotSlots.php
@@ -11,11 +11,22 @@
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 	if ($conn) {
-		$sql = "SELECT TOP (
-								SELECT CAST(1.1 * lot_capacity AS INT) AS lot_capacity
+		$sql = "SELECT CAST(1.1 * lot_capacity AS INT) AS lot_capacity
 								FROM g_lots
-								WHERE pk_id = '".$_POST['lot_id']."'
-							) pk_id, key_slot
+								WHERE pk_id = '".$_POST['lot_id']."'";
+		$res = sqlsrv_query($conn, $sql);
+		if ($res) {
+			while ($row = sqlsrv_fetch_array($res, SQLSRV_FETCH_ASSOC)) {
+				array_push($return_array, $row);
+			}
+		};
+		$cap = $return_array[0]['lot_capacity'];
+		while(($cap % 10) > 0) {
+			$cap++;
+		}
+		
+		$return_array = [];
+		$sql = "SELECT TOP (".$cap.") pk_id, key_slot
 				FROM key_slots
 				ORDER BY key_slot ASC";
 		$res = sqlsrv_query($conn, $sql);
